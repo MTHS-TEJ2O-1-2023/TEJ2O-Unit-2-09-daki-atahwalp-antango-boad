ef5a0c86eb637ecb009cfa08c7484b25cae56468
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-11 23:02:23 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,6 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onGesture(Gesture.Shake, function () {
-    
   randomNumber = randint(0, 2)
   basic.clearScreen()
 
@@ -30,12 +29,10 @@ input.onGesture(Gesture.Shake, function () {
     pause(5000)
     basic.showIcon(IconNames.Happy)
   }
-
 })
 
 // If randomNumber is 1
 if (randomNumber === 1) {
-
   basic.showLeds(`
 # # # # #
 # . . . #
@@ -45,16 +42,13 @@ if (randomNumber === 1) {
 `)
   pause(5000)
   basic.showIcon(IconNames.Happy)
-
 }
   
 // If randomNumber is 2
 if (randomNumber === 2) {
-
   basic.showIcon(IconNames.Scissors)
   basic.pause(5000)
   basic.showIcon(IconNames.Happy)
-
 }
 
 // Pause and show you are ready
@@ -63,21 +57,17 @@ basic.showIcon(IconNames.Happy)
 
 // On button A
 input.onButtonPressed(Button.A, function () {
-
   score = score + 1
   basic.showIcon(IconNames.Yes)
   basic.pause(1000)
   basic.showIcon(IconNames.Happy)
-
 })
 
 // On button B
 input.onButtonPressed(Button.B, function () {
-
   basic.clearScreen()
   basic.showString('Score : ')
   basic.showString(score.toString())
   basic.pause(1000)
   basic.showIcon(IconNames.Happy)
-
 })
